7274bce126f44e561ec0ee14e81f59ebaaa65c8a
John Zonneveld <jjczon@gmail.com>
2020-04-04 17:48:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,7 +14,9 @@ class TicTacToe
                         [6, 4, 2]]
 
     def display_board
-        puts " #{@board[0]} | #{@board[1]} | #{@board[2]}/n #{@board[3]} | #{@board[4]} | #{@board[5]}/n #{@board[6]} | #{@board[7]} | #{@board[8]}"
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
      
 
     end
